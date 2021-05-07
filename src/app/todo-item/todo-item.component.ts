e70902228a4062aa5e7b7be4aa8f180a645bad8a
Wilfred <wilfred05777@gmail.com>
2021-05-08 05:11:05 +0800

    tool tip and import

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { DataService } from '../shared/data.service';
 import { Todo } from '../shared/todo.model';
+import tippy from 'tippy.js';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss'],
 })
-export class TodoItemComponent implements OnInit {
+export class TodoItemComponent implements OnInit, AfterViewInit {
   // todo: Todo[];
   @Input() todo: Todo;
   // 2:16:57 in video  - fixed
@@ -15,11 +25,22 @@ export class TodoItemComponent implements OnInit {
   @Output() editClicked: EventEmitter<void> = new EventEmitter();
   @Output() deleteClicked: EventEmitter<void> = new EventEmitter();
 
+  @ViewChild('editBtn') editBtnElRef: ElementRef<HTMLElement>;
+
   constructor(private dataservice: DataService) {}
 
   ngOnInit(): void {
     // this.todos = this.dataservice.getAllTodos();
   }
+
+  ngAfterViewInit(): void {
+    console.log('This is the edit btn element');
+    console.log(this.editBtnElRef.nativeElement);
+    tippy(this.editBtnElRef.nativeElement, {
+      content: 'Edit Todo',
+    });
+  }
+
   onTodoClicked() {
     // console.log('Todo is clicked');
     this.todoClicked.emit();
